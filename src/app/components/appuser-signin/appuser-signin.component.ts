a7abf8ecce4c55c3c87f7fe5fb34b2787ecafc76
Hugo Duval <hugo-duval-1@hotmail.fr>
2023-01-02 19:20:32 +0100

    final commit

--- src/app/components/appuser-signin/appuser-signin.component.ts
+++ src/app/components/appuser-signin/appuser-signin.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {SessionStorageService} from "../../services/session-storage.service";
+import {AuthService} from "../../services/auth.service";
+import {AppUserService} from "../../services/app-user.service";
 
 @Component({
   selector: 'app-appuser-signin',
@@ -11,23 +14,47 @@ export class AppuserSigninComponent {
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', [Validators.required])
   });
+  unexistingEmail: boolean = true;
+  incorrectPassword: boolean = true;
 
-  constructor() {
+  constructor(private sessionStorageService: SessionStorageService,
+              private appUserService: AppUserService,
+              private authService: AuthService) {
   }
 
   seConnecter() {
     if (this.signingInAppUserForm.valid) {
-      const data = {
-        email: this.signingInAppUserForm.value.email,
-        password: this.signingInAppUserForm.value.password
+      const dataForm = {
+        email: <string>this.signingInAppUserForm.value.email,
+        password: <string>this.signingInAppUserForm.value.password
       };
 
-      console.log(data);
+      this.appUserService.getAll().subscribe({
+        next: (data) => {
+          data.forEach((value) => {
+            if (value['email'] == dataForm.email) {
+              this.unexistingEmail = false;
+              if (value['password'] == dataForm.password){
+                this.incorrectPassword = false;
+                this.authService.login(dataForm.email, dataForm.password);
+              } else {
+                console.log('Incorrect password'); // todo reactive, not here
+              }
+            } else {
+              console.log("This email adress doesn't exists"); // todo reactive, not here
+            }
+          })
+        },
+        error: (error) => {
+          console.log(error);
+        }
+      });
     }
   }
 
   private invalidField(field: string): boolean {
-    // @ts-ignore //todo : vérifier le @ts-ignore, pourquoi il veut un type string ou null ???
+    //todo : vérifier le @ts-ignore, pourquoi il veut un type string ou null ???
+    // @ts-ignore
     return this.signingInAppUserForm.controls[field].invalid && (this.signingInAppUserForm.controls[field].dirty || this.signingInAppUserForm.controls[field].touched);
   }
 
