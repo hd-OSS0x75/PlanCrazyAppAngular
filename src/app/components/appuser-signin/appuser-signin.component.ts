4e8798298d13b87aad8b758dd3919880ece6adf9
Hugo Duval <hugo-duval-1@hotmail.fr>
2023-01-02 15:31:44 +0100

    added basic typescript for the appuser-signin component

--- src/app/components/appuser-signin/appuser-signin.component.ts
+++ src/app/components/appuser-signin/appuser-signin.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-appuser-signin',
@@ -6,9 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./appuser-signin.component.css']
 })
 export class AppuserSigninComponent {
-  signingInAppUserForm: any;
+  signingInAppUserForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl('')
+  });
+
+  constructor() {
+  }
 
   seConnecter() {
+    const data = {
+      email: this.signingInAppUserForm.value.email,
+      password: this.signingInAppUserForm.value.password
+    };
 
+    console.log(data);
   }
 }
