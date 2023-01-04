03f04f9e67ef176979443e4df0e841f650454565
Hugo Duval <hugo-duval-1@hotmail.fr>
2023-01-04 07:25:18 +0100

    First html content for AppUser Homepage (month vision), added links and user nickname interpolation

--- src/app/components/calendar/app-user-homepage/app-user-homepage.component.ts
+++ src/app/components/calendar/app-user-homepage/app-user-homepage.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {SessionStorageService} from "../../../services/session-storage.service";
+import {AppUserService} from "../../../services/app-user.service";
 
 @Component({
   selector: 'app-app-user-homepage',
   templateUrl: './app-user-homepage.component.html',
   styleUrls: ['./app-user-homepage.component.css']
 })
-export class AppUserHomepageComponent {
+export class AppUserHomepageComponent implements OnInit {
+  nickname: string = 'Profil';
 
+  constructor(private sessionStorageService: SessionStorageService,
+              private appUserService: AppUserService) {}
+
+  ngOnInit(): void {
+    this.updateNickname(<string>this.sessionStorageService.getAppUserId());
+  }
+
+  private updateNickname(appUserId: string) {
+    this.appUserService.get(appUserId)
+      .subscribe({
+        next: value => {
+          this.nickname = value['nickname'];
+        },
+        error: err => {
+          console.log(err);
+        }
+      });
+  }
 }
