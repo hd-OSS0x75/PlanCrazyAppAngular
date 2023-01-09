6604eead79c636e954f7b273ed46fc75fc0efec2
Sego <segolenetricot@gmail.com>
2023-01-09 16:49:54 +0100

    API Calendar - divers mises en formes

--- src/app/components/calendar/full-calendar/full-calendar.component.ts
+++ src/app/components/calendar/full-calendar/full-calendar.component.ts
@@ -12,7 +12,6 @@ import {TaskService} from "../../../services/calendar/task.service";
 import {BehaviorSubject, Observable} from "rxjs";
 import {Task} from 'src/app/models/task';
 import {AddTaskComponent} from "../add-task/add-task.component";
-// import { INITIAL_EVENTS, createEventId } from '../../event-utils';
 let newTask: Task;
 
 @Component({
@@ -29,24 +28,42 @@ export class FullCalendarComponent implements OnInit {
 
   calendarOptions: CalendarOptions = {
     locale: frLocale,
+    timeZone: 'UTC',
+    initialView: 'dayGridMonth',
+    views: {
+      dayGridMonth: {
+        titleFormat: { month: 'long', year: 'numeric'}
+      }
+    },
+    contentHeight: 500,
+    dayMaxEventRows: true,
     plugins: [
       interactionPlugin,
       dayGridPlugin,
       timeGridPlugin,
       listPlugin,],
     events: this.taskList,
-    editable: true,
-    nowIndicator: true,
+    headerToolbar: {
+      start:'title',
+      end: 'today prev, next',
+    },
+    footerToolbar:{
+      center : "dayGridMonth,timeGridWeek,timeGridDay" //TODO : voir l'utilité
+    },
+    editable: true, // permet de déplacer la réunion en drag and drop //todo: la MAJ ne se fait pas en BDD
+    // eventDrop: (infos) => {
+    //   if(!confirm("Etes vous sûr de vouloir déplacer l'évènements?")) {infos.revert();}
+    //     },
+    nowIndicator: true, //TODO: essayer de le mettre en vert
     dateClick: this.handleDateSelect.bind(this),
     eventClick: this.handleEventClick.bind(this), //todo : make it work. What do we want from it ?
     eventColor: "#90B77D",
+    eventTimeFormat: { // like '14:30:00'
+      hour: '2-digit',
+      minute: '2-digit',
+      meridiem: false
+    },
   };
-
-
-  //BehaviorSubject est un observable et plus exactement un sous type de subject
-  //doit être initialisé avec une valeur par défaut afin de pouvoir tjs retourner une valeur aux observateurs
-  event$: BehaviorSubject<any> = new BehaviorSubject<any>([])
-
 
   constructor(private route: ActivatedRoute,
               private router: Router,
@@ -54,7 +71,7 @@ export class FullCalendarComponent implements OnInit {
               private taskService: TaskService) {
   }
 
-  ngOnInit(): void {
+ ngOnInit(): void {
     this.taskService.getAll().subscribe({
       next: value => {
         this.taskList = value.map(
@@ -77,59 +94,56 @@ export class FullCalendarComponent implements OnInit {
   private updateCalendar() {
     this.calendarOptions = {
       locale: frLocale,
+      timeZone: 'UTC',
+      initialView: 'dayGridMonth',
+      views: {
+        dayGridMonth: {
+          titleFormat: { month: 'long', year: 'numeric'}
+        }
+      },
+      contentHeight: 500,
+      dayMaxEventRows: true,
       plugins: [
         interactionPlugin,
         dayGridPlugin,
         timeGridPlugin,
         listPlugin,],
       events: this.taskList,
-      editable: true,
+      headerToolbar: {
+        start:'title',
+        end: 'today prev, next',
+      },
+      footerToolbar:{
+        center : "dayGridMonth,timeGridWeek,timeGridDay" //TODO : voir l'utilité
+      },
+      editable: true, // permet de déplacer la réunion en drag and drop //todo: la MAJ ne se fait pas en BDD
+      // eventDrop: (infos) => {
+      //   if(!confirm("Etes vous sûr de vouloir déplacer l'évènements?")) {infos.revert();}
+      //     },
       nowIndicator: true,
       dateClick: this.handleDateSelect.bind(this),
       eventClick: this.handleEventClick.bind(this), //todo : make it work. What do we want from it ?
       eventColor: "#90B77D",
+      eventTimeFormat: { // like '14:30:00'
+        hour: '2-digit',
+        minute: '2-digit',
+        meridiem: false
+      },
     };
   }
 
 //AU CLIC SUR UNE DATE PERMET DE MODIFIER LA VALEUR DE LA DATE
   handleDateSelect(selectDateInfo: DateClickArg) {
     let clickedDate = selectDateInfo.dateStr;
-    // console.log(clickedDate);
     this.choseThisDate(clickedDate);
-    //On entre des taches dans le calendrier
-    // const title = prompt(clickedDate + ' - Veuillez entrer un titre à votre tâche');
-    // const calendarApi = selectDateInfo.view.calendar;
-    // //  var addTaskComponent = new AddTaskComponent();
-    // calendarApi.unselect(); // clear date selection
-    // if (title) {
-    //   calendarApi.addEvent({
-    //     title,
-    //     start: selectDateInfo.dateStr,
-    //     end: selectDateInfo.dateStr,
-    //     allDay: selectDateInfo.allDay
-    //   });
-
-    // //ENREGISTREMENT EN BDD
-    // //Pour récupérer la date pour notre BDD il faut transformer la date du calendrier (string) en format date
-    // const[year, month, day] = clickedDate.split('-')
-    //  console.log(day);
-    //  console.log(month);
-    //  console.log(year);
-    //  const date = new Date(+year, +month - 1, +day);
-    //  console.log(date);
-    //  newTask.startingDate = date;
-    //  newTask.taskTitle = title;
-    //  console.log(newTask.taskTitle);
-    // this.taskService.add(newTask);
   }
 
+  //TODO: reprendre cette fonction qui ne fonctionne pas
   //AU CLIC SUR UN EVENEMENT, UNE TACHE ON A SON DETAIL
   handleEventClick(clickInfo: EventClickArg) {
-    //QUAND ON CLIQUE SUR UN EVENEMENT on affiche son nom dans la console ou son id
     console.log(clickInfo.event.title)
     console.log(clickInfo.event.id)
     console.log(clickInfo)
-
     this.taskService.get(clickInfo.event.id).subscribe({
       next: () => this.router.navigate(['/details/:clickInfo.event.id']),
       error: (err) => console.log(err)
