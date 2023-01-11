10d6c9cc0dbfd2192e076fac16eb8e3875bfd6cc
Hugo Duval <hugo-duval-1@hotmail.fr>
2023-01-11 11:47:58 +0100

    added utility component for displaying an user which has a task shared with

---
+++ src/app/components/calendar/utilities/user-task-is-shared-with/user-task-is-shared-with.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-task-is-shared-with',
+  templateUrl: './user-task-is-shared-with.component.html',
+  styleUrls: ['./user-task-is-shared-with.component.css']
+})
+export class UserTaskIsSharedWithComponent {
+
+}
