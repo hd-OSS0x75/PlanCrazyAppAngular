8cd43a46290444edf3c405c262ec0296db2122f0
Hugo Duval <hugo-duval-1@hotmail.fr>
2023-01-11 12:45:56 +0100

    first implementation of the list of user whose a task is shared with

--- src/app/components/calendar/utilities/user-task-is-shared-with/user-task-is-shared-with.component.ts
+++ src/app/components/calendar/utilities/user-task-is-shared-with/user-task-is-shared-with.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 
 @Component({
   selector: 'app-user-task-is-shared-with',
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-task-is-shared-with.component.css']
 })
 export class UserTaskIsSharedWithComponent {
+  @Input('email')
+  appUserEmail?: string;
 
+  @Output()
+  unshareWithThisUserEvent = new EventEmitter<string>();
+
+  unShareWithThisUser() {
+    this.unshareWithThisUserEvent.emit(this.appUserEmail);
+  }
 }
