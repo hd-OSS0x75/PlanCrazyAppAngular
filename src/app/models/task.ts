1e583cf84924c0f548fd9919487bb39178479236
Hugo Duval <hugo-duval-1@hotmail.fr>
2023-01-12 09:16:45 +0100

    task model : added owner email field

--- src/app/models/task.ts
+++ src/app/models/task.ts
@@ -1,11 +1,12 @@
 export interface Task {
-    taskId?: string;
-    taskTitle: string;
-    description: string;
-    location: string;
-    startingDate: Date;
-    startingHour: any;
-    endingDate: Date;
-    endingHour: any;
-    private: boolean;
+  taskId?: string;
+  taskTitle: string;
+  description: string;
+  location: string;
+  startingDate: Date;
+  startingHour: any;
+  endingDate: Date;
+  endingHour: any;
+  private: boolean;
+  ownerEmail?: string;
 }
